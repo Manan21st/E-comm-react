9234cb44f630a2935ddf624c966cf5cfea00a061
Manan21st <manan9346@gmail.com>
2024-06-26 14:11:59 +0530

    cart created

---
+++ src/components/Cart/cart.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {useSelector} from "react-redux";
+
+function Cart() {
+    const cartElements = useSelector(state => state.items); 
+    return (
+        <div>
+            {
+                Object.keys(cartElements).map((key) => {
+                    return (
+                        <div className="currentCart">
+                            <div>{cartElements[key].title}</div>
+                            <div> {cartElements[key].quantity}</div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    );
+}
